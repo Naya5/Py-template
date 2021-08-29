ea473b5fd856af310a9f261fc14f07bea1cc52b7
Naya5 <Naya5@users.noreply.github.com>
2021-08-29 01:21:16 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Naya5"
+original_name="py_template"
+original_urlname="Py-template"
+original_description="Awesome py_template created by Naya5"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv py_template $name
 
 # This command runs only once on GHA!
 if [ ! -f .github/template.log ]; then
